ab67163fa48777175f95b83a4ce78a3c7b8f2e4b
Babasahebbhise12 <bhisebabasaheb001@email.com>
2023-09-24 19:27:53 +0530

    Creatin_Project_Template_With_Register_and_Login_Page

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HOTELBHISEFAMILY.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace HOTELBHISEFAMILY.Models
+{
+    public class Customer
+    {
+        public int Customer_Id { get; set; }
+        public string Customer_First_Name { get; set;}
+        public string Customer_Middle_Name { get; set; }
+        public string Customer_Last_Name { get; set; }
+        public string Customer_Email { get; set;}
+        public string Customer_MobileNumber { get; set;}
+        public string Customer_City { get; set;}
+        public string Customer_Password { get; set; }
+        public IFormFile Customer_ProfilePhote { get; set; }
+
+    }
+}
